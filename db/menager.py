fdf618bc3daf8bf84992e12ca2541805a0d2114a
Vlad Haidaichuk <vlad.haidaichuk@gmail.com>
2023-11-25 18:50:48 +0200

    build | edit build

--- db/menager.py
+++ db/menager.py
@@ -1,6 +1,5 @@
 from pymongo.mongo_client import MongoClient
 from conf import DB_TOKEN, DB_USER
-import csv
 
 
 cluster = "cluster0"
@@ -8,92 +7,5 @@ uri = f"mongodb+srv://{DB_USER}:{DB_TOKEN}@{cluster}.gbcrwrb.mongodb.net/?retryW
 client = MongoClient(uri)
 customer_db = client["customer"]
 users_collection = customer_db["users"]
-# users_collection = customer_db["test"]
-
-csvfile = open('lib/Users.csv', 'r', newline='')
-csvreader = csv.reader(csvfile)
-logins_data = [row[1] for row in csvreader]
-logins_data = logins_data[1:]
-
-csvfile = open('lib/Users.csv', 'r', newline='')
-csvreader = csv.reader(csvfile)
-numbers_data = [row[2] for row in csvreader]
-numbers_data = numbers_data[1:]
-
-csvfile = open('lib/Users.csv', 'r', newline='')
-csvreader = csv.reader(csvfile)
-email_data = [row[6] for row in csvreader]
-email_data = email_data[1:]
-
-csvfile = open('lib/Users.csv', 'r', newline='')
-csvreader = csv.reader(csvfile)
-full_name_data = [row[3] for row in csvreader]
-full_name_data = full_name_data[1:]
-
-csvfile = open('lib/Users.csv', 'r', newline='')
-csvreader = csv.reader(csvfile)
-group_data = [row[4] for row in csvreader]
-group_data = group_data[1:]
-group_result = list()
-for group in group_data:
-    group = int(group)
-    if group == 0:
-        group_result.append("10A")
-    if group == 1:
-        group_result.append("10Б")
-    if group == 2:
-        group_result.append("10В")
-    if group == 3:
-        group_result.append("10Г")
-group_data = group_result
-
-
-class User:
-    users = list()
-
-    def build_user(self, number, full_name, absence, login, email, group, attendance=0, telegram_id=None):
-        user = {
-             "info": {
-                "number": number,
-                "telegram_id": telegram_id,
-                "full_name": full_name,
-                "login": login,
-                "email": email,
-                "group": group,
-
-             },
-             "status": {
-                 "attendance": attendance,
-                 "absence": absence,
-             },
-             "marks": {
-                "login": None,
-                 "password": None,
-                 "last_update": None,
-                 "marks": [],
-             }
-
-
-        }
-        self.users.append(user)
-
-    def get_all_users(self):
-        return self.users
-
-
-user = User()
-
-for i in range(len(logins_data)):
-    user.build_user(
-        number=numbers_data[i],
-        full_name=full_name_data[i],
-        login=logins_data[i],
-        email=email_data[i],
-        group=group_data[i],
-        absence=[]
-    )
-
-
-# users_collection.insert_many(user.get_all_users())
 
 print("Db status is", customer_db.command('ping'))
